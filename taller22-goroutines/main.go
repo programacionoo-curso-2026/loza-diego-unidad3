ea2fc2515781045527c76579a840edac68d7cf35
Usuario Internacional <usuario@uide.edu.ec>
2026-07-27 10:39:14 -0500

    sexta  iteracion

--- taller22-goroutines/main.go
+++ taller22-goroutines/main.go
@@ -34,3 +34,51 @@ func generateOrders(count int) []*Order {
   }
   return orders
 }
+func processOrders(orders []*Order) {
+  for _, order := range orders {
+    time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+    fmt.Printf("Procesando orden %d\n", order.ID)
+  }
+}
+func updateOrderStatuses(orders []*Order) {
+  for _, order := range orders {
+    time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+    status := []string{
+      "Procesando", "Despachando", "Entregado",
+    }[rand.Intn(3)]
+    order.Status = status
+    fmt.Printf("Actualizando orden %d con estado: %s\n",
+      order.ID, status)
+  }
+}
+func reportOrderStatus(orders []*Order) {
+  for i := 0; i < 5; i++ {
+    time.Sleep(1 * time.Second)
+    fmt.Printf("\n--- Reporte Estado de las Ordenes ---\n")
+    for _, order := range orders {
+      fmt.Printf("Orden %d %s\n",
+        order.ID, order.Status)
+    }
+    fmt.Printf("---------------------------------------\n")
+  }
+}
+func main() {
+  orders := generateOrders(20)
+  processOrders(orders)
+  updateOrderStatuses(orders)
+  reportOrderStatus(orders)
+  fmt.Print("Todas las operaciones completadas. Finalizando\n")
+}
+func main() {
+
+  orders := generateOrders(20)
+
+    go processOrders(orders)
+
+  go updateOrderStatuses(orders)
+
+    go reportOrderStatus(orders)
+
+  fmt.Print("Todas las operaciones completadas. Saliendo")
+
+}
